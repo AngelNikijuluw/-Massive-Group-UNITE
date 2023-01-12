f3fcb17bad10739f8ddcb638f518f6d4503121d8
FahriEfendi~ <fahriefendi0795@gmail.com>
2023-01-12 21:55:43 +0700

    jun 12 update

--- frontend/src/pages/Dashboard/QRCode.js
+++ frontend/src/pages/Dashboard/QRCode.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react'
 import { Container } from 'react-bootstrap';
 import TopBAr from '../../components/TopBar';
 import { Card,Col,Row,Button } from 'react-bootstrap';
 import Sidebar from '../../components/Sidebar';
-const QRCode = () =>{
+import {Fab, TextareaAutosize} from '@material-ui/core'
+import {ArrowBack} from '@material-ui/icons'
+import { Link } from "react-router-dom";
+import QrReader from 'react-web-qr-reader';
+
+function QRCode (){
+  const [data, setdata] = useState('No result');
+  const handleScan = data => {
+      if (data) {
+          setdata(data)
+      }
+  }
+  const handleError = err => {
+  console.error(err)
+  }
     return (
         <div  style={{backgroundColor:"#EEF1FF"}}>
             <TopBAr/>
-            <Sidebar />
             <Container>
-            
+            <Sidebar/>
             <Row>
         <Col md={{ span: 12, offset: 1}}><Card style={{backgroundColor:"#AAC4FF",marginTop:"60px"}}>
       <Card.Body >
@@ -21,7 +34,28 @@ const QRCode = () =>{
     </Card></Col>
         <Col md={{ span: 12, offset: 1 }}>
         <Card Card style={{backgroundColor:"#AAC4FF",height:"500px",marginTop:"20px"}}>
-        <h1>Qr Code</h1>
+        <div>
+            <Link to="/">
+            <Fab style={{marginRight:10}} color="primary">
+                <ArrowBack/>
+            </Fab>
+            </Link>
+            <span>QR Scanner</span>
+            
+            <center>
+            <div style={{marginTop:30}}>
+                <QrReader
+                    delay={300}
+                    onError={handleError}
+                    onScan={handleScan}
+                    style={{ height: 30, width: 320}}
+                />
+            </div>
+            </center>
+
+            
+
+      </div>
         </Card>
         </Col>
     
